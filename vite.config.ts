db90ca9a4018570981bc6e29a326786c90ccf6ef
Anuragspace <anuraglife2020@gmail.com>
2026-06-30 17:43:07 +0530

    Install and configure @netlify/vite-plugin-tanstack-start

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   plugins: [
@@ -10,5 +11,6 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "src/server.ts" },
     }),
+    netlify(),
   ],
 });
